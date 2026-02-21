93f0857cd2f4fca1be2aad8594e790e4b2b957d6
mustafaelshahhat-art <mustafaelshahhat@gmail.com>
2026-02-21 03:42:46 +0200

    fix: update API and hub URLs in production environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://korazon365.runasp.net/api/v1',
-    imageBaseUrl: 'https://korazon365.runasp.net',
-    hubUrl: 'https://korazon365.runasp.net/hubs',
+    apiUrl: 'https://korazone365-gsa2ftfkh9atbuaw.francecentral-01.azurewebsites.net/api/v1',
+    imageBaseUrl: 'https://korazone365-gsa2ftfkh9atbuaw.francecentral-01.azurewebsites.net',
+    hubUrl: 'https://korazone365-gsa2ftfkh9atbuaw.francecentral-01.azurewebsites.net/hubs',
     appName: 'Kora Zone 365',
     version: '1.0.0',
     enableDebugTools: false,
